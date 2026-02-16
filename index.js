2d2ce1e2dfd97a120ca613404afc5b5420cf20c2
Shivam Mishra <mshivam2062@gmail.com>
2026-02-16 20:52:18 +0530

    Add Server side apporch

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
 io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id)
+  console.log(`Socket Connected`, socket.id);
   socket.on("room:join", (data) => {
     const { email, room } = data;
-    emailToSocketIdMap.set(email, socket.id)
-    socketidToEmailMap.set(socket.id, email)
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
     io.to(room).emit("user:joined", { email, id: socket.id });
     socket.join(room);
     io.to(socket.id).emit("room:join", data);
